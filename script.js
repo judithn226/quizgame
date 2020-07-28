176b67136677ba54ee1c2d4285cdff1df226c4de
Judith Nwagwu <Judith5nwagwu@gmail.com>
2020-07-28 10:47:22 -0700

    starting code

---
+++ script.js
@@ -0,0 +1,7 @@
+const quizholder = document.getElementById('quiz')
+const resultsholder = document.getElementById('results')
+const submitBtn = document.getElementById('submit')
+
+function makeQuiz(){}
+function showResults(){}
+submitBtn.addEventListener('click', showResults)
